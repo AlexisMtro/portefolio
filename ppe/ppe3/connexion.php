df51860123f3b4e306d8a67bbcfc3b27d709b0fc
Alexis Motro <alexismotro@gmail.com>
2018-12-08 03:25:33 +0100

    projet init

---
+++ ppe/ppe3/connexion.php
@@ -0,0 +1,10 @@
+<?php include 'redirectConnexionResponsable.php';
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=alexismopgppegsb;charset=utf8', 'root', '');
+}
+catch(Exception $e) {
+    die('Erreur : '.$e->getMessage());
+}
+
+?>
